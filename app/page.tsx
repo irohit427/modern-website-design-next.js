99419e88730930507d8fd7d7dab1891192670387
Rohit <irohit427@users.noreply.github.com>
2023-08-18 00:56:03 +0530

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
-import { Fragment } from 'react'
+import styles from './page.module.css';
+import Hero from '../public/hero.png'
 
 export default function Home() {
   return (
-    <Fragment>
-      <h1>Home</h1>
-    </Fragment>
+    <div className={styles.container}>
+      <div className={styles.bannerItems}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>Turning ideas into reality. We bring together teams from the 
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.bannerItems}>
+        <Image src={Hero} alt='hero' className={styles.bannerImg} />
+      </div>
+    </div>
   )
 }
