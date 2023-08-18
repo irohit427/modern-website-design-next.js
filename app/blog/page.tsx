56b67c3d6e74b8be76981d092f4062263f42e7fb
Rohit <irohit427@users.noreply.github.com>
2023-08-18 13:25:26 +0530

    Blog Post

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import styles from './page.module.css';
 
-const Blog = () => {
+const getData = async() => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+const Blog = async () => {
+
+  const data:any = await getData();
+  console.log(data);
+  
   return (
-    <div>Blog</div>
-  )
+    <div className={styles.mainContainer}>
+      {data.map((item: any) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Blog
---
+++ utils/connectDB.ts
@@ -0,0 +1,1 @@
+// Connnec to Mongo
